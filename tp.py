9524dad1a9ebd10c46a1b89c5c5db6b37f91b294
julianfonti <julian_cafi@hotmail.com>
2025-05-23 18:28:13 -0300

    Def para carga y generación lista materias aprobadas

--- tp.py
+++ tp.py
@@ -3,52 +3,6 @@
 
 # Trabajo Practico -
 
-# Funcion para cargar materias aprobadas
-def Cargar_materias_aprobadas(materias):
-    materias_aprobadas = []
-    for i in range(len(materias)):
-        aprobo = ingresar_y_validar_aprobado(materias[i][1])
-        if aprobo == "si":
-            nota = ingresar_y_validar_nota(materias[i][1])
-            if nota != 0:
-                materia_aprobada = [materias[i][0], materias[i][1], nota]
-
-    return materias_aprobadas
-
-
-def ingresar_y_validar_nota(nombre_materia):
-    nota = int(input("ingrese la nota de la materia " + nombre_materia + ": "))
-    while nota < 0 or nota > 10:
-        print("La nota ingresada no es válida. Ingrese nuevamente la nota de la materia " +
-              nombre_materia + ":")
-        nota = int(
-            input("ingrese la nota de la materia " + nombre_materia + ": "))
-    if nota < 4:
-        print("La nota ingresada es menor a 4, por lo tanto no se considera aprobada la materia " + nombre_materia)
-        nota = 0
-    return nota
-
-
-def ingresar_y_validar_aprobado(nombre_materia):
-    aprobado = input("¿Aprobaste la materia " + nombre_materia + "? (si/no): ")
-    while aprobado != "si" and aprobado != "no":
-        print("La respuesta ingresada no es válida. Ingrese nuevamente si aprobó la materia " + nombre_materia + ":")
-        aprobado = input()
-    return aprobado
-
-# Valores puestos en array de las materias universitarias
-
-
-def listas_de_materias(materias):
-    nombres = []
-    cantidad_de_materias_totales = len(materias)
-    index = 0
-    while index < cantidad_de_materias_totales:
-        nombre_materia = materias[index][1]
-        nombres.append(nombre_materia)
-        index = index + 1
-    return nombres
-
 
 # Acá van a ir y todas las funciones que se van a utilizar para el programa
 # ? Funcion que reciba por parametro ID con las materias cursadas (en base al input del usuario) y devuelve como lista la lista de materias filtradas
@@ -63,13 +17,58 @@ def programa_restante_post_ingreso_de_materias(lista_con_materias_aprobadas, lis
             materias_restantes.append(lista_materias[i])
     return materias_restantes
 
+#Funcion que registra las materias ingresadas por el usuario como aprobadas. Verifica que esté en lista materias, en caso de ser correcto la almacena.
+#Para finalizar con el ingreso de materias aprobadas ingresa -1.
+# Lista vacía donde se almacenarán los códigos de materias aprobadas
+lista_aprobadas = []
+
+# Función para registrar materias aprobadas
+
+def registrar_materias_aprobadas(materias, lista_aprobadas):
+    print("Ingresá los códigos de las materias que aprobaste.")
+    print("Escribí -1 para terminar.")
+
+    salir = False  # Bandera de control para salir de ciclo while.
+
+    while salir == False:
+        codigo = input("Código de materia aprobada: ")
+
+        if codigo == "-1":
+            salir = True  # Activamos la bandera para salir del while.
+        else:
+            i = 0
+            materia_encontrada = False
+
+            # Buscamos si el código está en la lista de materias
+            while i < len(materias):
+                if materias[i][0] == codigo:
+                    materia_encontrada = True
+
+                    # Verificamos si ya está en la lista de aprobadas
+                    j = 0
+                    ya_aprobada = False
+                    while j < len(lista_aprobadas):
+                        if lista_aprobadas[j] == codigo:
+                            ya_aprobada = True
+                        j = j + 1
+
+                    if ya_aprobada == False:
+                        lista_aprobadas.append(codigo)
+                        print("Materia registrada como aprobada:", materias[i][1])
+                    else:
+                        print("Ya registraste esta materia.")
+                i = i + 1
+
+            if materia_encontrada == False:
+                print("Código no encontrado en la lista de materias.")
+
 
 # Funcion que reciba por parametro ID con materia cursada previamente para evaluar si puede cursar una materia siguiente (Puede devolver un booleano como true - false)
 
 
 def habilitado_para_cursar(lista_con_materias_aprobadas, id_materia_que_quiero_cursar):
     print("Hay que crear la funcion que devuelve si estoy en condiciones de cursar o no la materia")
-
+#JULI VOY A VER DE HACER DEF PARA VERIFIAR SI SE PUEDE CURSAR LA MATERIA INGRESADA.
 
 # Funcion que reciba por parametro ID con la materia que quiere cursar y devuelve una lista de materias que necesita cursar previamente para poder cursar la materia deseada.
 
@@ -108,12 +107,14 @@ def materias_online(lista_materias):
 
 
 # Funcion que devuelve si una materia con ID (pasado por parametro) puede ser cursada por online o no
+    #JULI VOY A VER DE HACER DEF PARA VERIFIAR SI SE PUEDE CURSAR LA MATERIA INGRESADA.
+
 
 
 def materia_online_id(id):
 
     print("Devuelve si una materia se puede cursar online o no")
-
+#JULI Crear funcion para verificar si la materia se puede hacer online.
 
 # Valores puestos en array de las materias universitarias
 def listas_de_materias(materias):
@@ -198,8 +199,13 @@ materias = [
     ["2.3.056", "Derecho Informático", [], [], True]
 ]
 
-materias_necesarias_para_titulo_intermedio = ['3.4.069', '3.4.164', '2.1.002', '3.4.043', '3.1.050', '3.4.071', '3.3.121', '3.2.178', '3.4.072', '3.1.024', '3.1.051', '3.4.074', '3.4.207', '3.4.075',
-                                              '3.1.052', '3.1.053', '3.4.077', '3.4.208', '3.4.078', '3.4.209', '3.1.054', '3.4.210', '3.4.211', '3.4.212', '3.4.213', '3.1.049', '2.4.216']
+materias_harcodeadas_aprobadas = [
+    ["3.4.069", "Fundamentos de Informática", [], ["3.4.071"], True],
+    ["3.4.164", "Sistemas de Información I", [], ["3.4.207"], True],
+    ["2.1.002", "Pensamiento Crítico y Comunicación", [], [], True],
+    ["3.4.043", "Teoría de Sistemas", [], [], True],
+    ["3.1.050", "Elementos de Álgebra y Geometría", [], ["3.1.051"], False],
+]
 
 
 programa_inicial = print(
@@ -225,6 +231,7 @@ while opcion_elegida != -1:
         print("3: Ver que materias tengo que tener aprobadas, para cursar una materia")
         print("4: Ver listado de materias online")
         print("5: Ver si una materia esta disponible para cursar online")
+        #ver si agregamos la función de promedio.
 
         opcion_elegida = int(input())
 
