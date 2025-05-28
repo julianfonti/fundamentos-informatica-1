032380beb47ed6c5966a6f411f0dec170bfcd65e
juanetchart29 <juanetchart29@gmail.com>
2025-05-27 21:13:23 -0300

    funcion main updates

--- tp.py
+++ tp.py
@@ -24,7 +24,7 @@ lista_aprobadas = []
 
 # Función para registrar materias aprobadas
 
-def registrar_materias_aprobadas(materias, lista_aprobadas):
+def registrar_materias_aprobadas(materias, lista_aprobadas):# puedo cambiar el while para salir si ingresa -1 ?
     print("Ingresá los códigos de las materias que aprobaste.")
     print("Escribí -1 para terminar.")
 
@@ -181,74 +181,70 @@ def listas_de_materias(materias):
 
 # formateo de lista
 
-def formateo_lista(lista):
+def recorrer_plan_completo(lista):
     largo = len(lista)
     for i in range(largo):
-        print(lista[i], end="")
+        print(lista[i][1], end="")# printeo unicamente el nombre que esta en la segunda posicion de cada lista
         print()
-    return
-
+    
 
 # hay que setear el ultimo booleano (update: Listo, esta en base a la lista igual de licenciatura habria que evaluar que materias faltan de la ingenieria) jp:espectacular :)
-materias = [
-    ["3.4.069", "Fundamentos de Informática", [], ["3.4.071"], True],
-    ["3.4.164", "Sistemas de Información I", [], ["3.4.207"], True],
-    ["2.1.002", "Pensamiento Crítico y Comunicación", [], [], True],
-    ["3.4.043", "Teoría de Sistemas", [], [], True],
-    ["3.1.050", "Elementos de Álgebra y Geometría", [], ["3.1.051"], False],
-    ["3.4.071", "Programación I", ["3.4.069"], ["3.4.074", "3.4.208"], False],
-    ["3.3.121", "Sistemas de Representación", [], [], True],
-    ["3.2.178", "Fundamentos de Química", [], [], True],
-    ["3.4.072", "Arquitectura de Computadores", [], ["3.4.075"], True],
-    ["3.1.024", "Matemática Discreta", [], ["3.4.209", "3.4.215"], True],
-    ["3.1.051", "Álgebra", ["3.1.050"], ["3.1.052"], False],
-    ["3.4.074", "Programación II", ["3.4.071"], ["3.4.077", "3.4.211"], True],
-    ["3.4.207", "Sistemas de Información II", ["3.4.164"], [
-        "3.4.214", "3.4.211", "3.4.089", "3.4.094"], True],
-    ["3.4.075", "Sistemas Operativos", ["3.4.072"], [], True],
-    ["3.1.052", "Física I", ["3.1.051"], ["3.1.055"], False],
-    ["3.1.053", "Cálculo I", [], ["3.1.054", "3.1.049"], True],
-    ["3.4.077", "Programación III", ["3.4.074"], ["3.4.215"], False],
-    ["3.4.208", "Paradigma Orientado a Objetos", [
-        "3.4.071"], ["3.4.210", "3.4.082"], True],
-    ["3.4.078", "Fundamentos de Telecomunicaciones", [], ["3.4.212"], True],
-    ["3.4.209", "Ingeniería de Datos I", ["3.1.024"], ["3.4.213", "3.4.211"], False],
-    ["3.1.054", "Cálculo II", ["3.1.053"], ["3.1.025"], False],
-    ["3.4.210", "Proceso de Desarrollo de Software",
-        ["3.4.208"], ["3.4.216", "3.4.218"], False],
-    ["3.4.211", "Seminario de Integración Profesional",
-        ["3.4.074", "3.4.207", "3.4.209"], [], False],
-    ["3.4.212", "Teleinformática y Redes", ["3.4.078"], ["3.4.092"], False],
-    ["3.4.213", "Ingeniería de Datos II", ["3.4.209"], ["3.4.217"], False],
-    ["3.1.049", "Probabilidad y Estadística", ["3.1.053"],
-        ["3.1.056", "3.4.086", "3.4.217"], False],
-    ["2.4.216", "Examen de Inglés", [], [], True],
-    ["3.4.082", "Aplicaciones Interactivas", ["3.4.208"], ["3.4.218"], False],
-    ["3.4.214", "Ingeniería de Software", ["3.4.207"], ["3.4.098"], False],
-    ["3.1.055", "Física II", ["3.1.052"], [], False],
-    ["3.4.215", "Teoría de la Computación", ["3.1.024", "3.4.077"], [], True],
-    ["3.1.056", "Estadística Avanzada", ["3.1.049"], ["3.4.096"], False],
-    ["3.4.216", "Desarrollo de Aplicaciones I", ["3.4.210"], [], False],
-    ["3.4.089", "Dirección de Proyectos Informáticos", ["3.4.207"], [], False],
-    ["3.4.217", "Ciencia de Datos", ["3.1.049", "3.4.213"], [], False],
-    ["3.4.092", "Seguridad e Integridad de la Información", ["3.4.212"], [], False],
-    ["3.1.025", "Modelado y Simulación", ["3.1.054"], [], False],
-    ["OPT1", "Optativa I", [], [], True],
-    ["3.4.218", "Desarrollo de Aplicaciones II", ["3.4.082", "3.4.210"], [], False],
-    ["3.4.086", "Evaluación de Proyectos Informáticos", ["3.1.049"], [], False],
-    ["3.4.096", "Inteligencia Artificial", ["3.1.056"], [], False],
-    ["3.4.219", "Tecnología y Medio Ambiente", [], [], True],
-    ["PPS06", "Práctica Profesional Supervisada", [], [], True],
-    ["OPT2", "Optativa II", [], [], True],
-    ["3.4.094", "Arquitectura de Aplicaciones", ["3.4.207"], [], False],
-    ["3.4.220", "Tendencias Tecnológicas", [], [], True],
-    ["3.4.100", "Proyecto Final de Ingeniería en Informática", [], [], True],
-    ["3.4.098", "Calidad de Software", ["3.4.214"], [], False],
-    ["OPT3", "Optativa III", [], [], True],
-    ["3.4.221", "Negocios Tecnológicos", [], [], True],
-    ["3.4.135", "Tecnología e Innovación", [], [], True],
-    ["2.3.056", "Derecho Informático", [], [], True]
+materias = materias = [
+    [1, "Fundamentos de Informática", [], [6], True],
+    [2, "Sistemas de Información I", [], [13], True],
+    [3, "Pensamiento Crítico y Comunicación", [], [], True],
+    [4, "Teoría de Sistemas", [], [], True],
+    [5, "Elementos de Álgebra y Geometría", [], [11], False],
+    [6, "Programación I", [1], [12, 18], False],
+    [7, "Sistemas de Representación", [], [], True],
+    [8, "Fundamentos de Química", [], [], True],
+    [9, "Arquitectura de Computadores", [], [14], True],
+    [10, "Matemática Discreta", [], [19, 30], True],
+    [11, "Álgebra", [5], [15], False],
+    [12, "Programación II", [6], [17, 22], True],
+    [13, "Sistemas de Información II", [2], [29, 22, 35, 42], True],
+    [14, "Sistemas Operativos", [9], [], True],
+    [15, "Física I", [11], [33], False],
+    [16, "Cálculo I", [], [20, 26], True],
+    [17, "Programación III", [12], [30], False],
+    [18, "Paradigma Orientado a Objetos", [6], [21, 28], True],
+    [19, "Fundamentos de Telecomunicaciones", [], [24], True],
+    [20, "Ingeniería de Datos I", [10], [25, 22], False],
+    [21, "Cálculo II", [16], [36], False],
+    [22, "Proceso de Desarrollo de Software", [18], [32, 38], False],
+    [23, "Seminario de Integración Profesional", [12, 13, 20], [], False],
+    [24, "Teleinformática y Redes", [19], [34], False],
+    [25, "Ingeniería de Datos II", [20], [37], False],
+    [26, "Probabilidad y Estadística", [16], [40, 45, 37], False],
+    [27, "Examen de Inglés", [], [], True],
+    [28, "Aplicaciones Interactivas", [18], [38], False],
+    [29, "Ingeniería de Software", [13], [47], False],
+    [30, "Física II", [15], [], False],
+    [31, "Teoría de la Computación", [10, 17], [], True],
+    [32, "Estadística Avanzada", [26], [44], False],
+    [33, "Desarrollo de Aplicaciones I", [22], [], False],
+    [34, "Dirección de Proyectos Informáticos", [13], [], False],
+    [35, "Ciencia de Datos", [26, 25], [], False],
+    [36, "Seguridad e Integridad de la Información", [24], [], False],
+    [37, "Modelado y Simulación", [21], [], False],
+    [38, "Optativa I", [], [], True],
+    [39, "Desarrollo de Aplicaciones II", [28, 22], [], False],
+    [40, "Evaluación de Proyectos Informáticos", [26], [], False],
+    [41, "Inteligencia Artificial", [32], [], False],
+    [42, "Tecnología y Medio Ambiente", [], [], True],
+    [43, "Práctica Profesional Supervisada", [], [], True],
+    [44, "Optativa II", [], [], True],
+    [45, "Arquitectura de Aplicaciones", [13], [], False],
+    [46, "Tendencias Tecnológicas", [], [], True],
+    [47, "Proyecto Final de Ingeniería en Informática", [], [], True],
+    [48, "Calidad de Software", [29], [], False],
+    [49, "Optativa III", [], [], True],
+    [50, "Negocios Tecnológicos", [], [], True],
+    [51, "Tecnología e Innovación", [], [], True],
+    [52, "Derecho Informático", [], [], True]
 ]
+
+    
 
 materias_harcodeadas_aprobadas = [
     ["3.4.069", "Fundamentos de Informática", [], ["3.4.071"], True],
@@ -258,55 +254,60 @@ materias_harcodeadas_aprobadas = [
     ["3.1.050", "Elementos de Álgebra y Geometría", [], ["3.1.051"], False],
 ]
 
+def recorrer_menu():
+    print("Ingrese una opción del menú:")
+    for i in range(len(Lista_menu)):
+        print(Lista_menu[i])
+    
 
-programa_inicial = print(
-    "Bienvenidos al programa, por favor indique una de las siguientes opciones o -1 para finlalizar el programa")
-
-print("1: Ver plan de estudios completo")
-print("2: Ingresar materias aprobadas para ver cuales restan cursar")
-print("3: Ver que materias tengo que tener aprobadas, para cursar una materia")
-print("4: Ver listado de materias online")
-print("5: Ver si una materia esta disponible para cursar online")
+Lista_menu = [
+    "1: Ver plan de estudios completo",
+    "2: Ingresar materias aprobadas para ver cuales restan cursar",
+    "3: Ver que materias tengo que tener aprobadas, para cursar una materia",
+    "4: Ver listado de materias online",
+    "5: Ver si una materia esta disponible para cursar online"]
 #ver si agregamos la función de promedio.
 #ver si sumamos la opcion de mostrar nota en materia ya aprobada
 
+def main(materias):
+    materias_aprobadas = []
+    print("Bienvenido al sistema de consulta de materias de la carrera de Ingeniería en Informática.")
+    recorrer_menu()
 
-opcion_elegida = int(input())
-# Ejemplo de uso:
-
-while opcion_elegida != -1:
-    # Bucle por si se ingresa un valor fuera de rango. Se solicita que se reingrese una opcion válida
-    while opcion_elegida <1 or opcion_elegida >5:
-
-        print('Disculpe, la opcion elegída no es válida, ingrese un numero del 1 al 5 según lo que desee consultar.')
-        print("1: Ver plan de estudios completo")
-        print("2: Ingresar materias aprobadas para ver cuales restan cursar")
-        print("3: Ver que materias tengo que tener aprobadas, para cursar una materia")
-        print("4: Ver listado de materias online")
-        print("5: Ver si una materia esta disponible para cursar online")
-        #ver si agregamos la función de promedio.
-
+    opcion_elegida = int(input())
+    while opcion_elegida != -1 and (opcion_elegida <1 or opcion_elegida > len(Lista_menu)):
+        print("Opción no válida. Por favor, ingrese una opción del menú:")
+        recorrer_menu()
         opcion_elegida = int(input())
 
+    menu_seleccionado(opcion_elegida, materias)
+
+
+def menu_seleccionado(opcion_elegida, materias):
     if opcion_elegida == 1:
-        resultado = listas_de_materias(materias)
-        print(formateo_lista(resultado))
+        # resultado = listas_de_materias(materias)
+        # print(formateo_lista(resultado))
+        recorrer_plan_completo(materias)
 
     if opcion_elegida == 2:
         print('Ingrese las materias aprobadas para ver cuales restan cursar')
+        registrar_materias_aprobadas(materias, materias_aprobadas)
         materias_aprobadas = int(float())
         resultado = listas_de_materias(materias)
-        print(formateo_lista(resultado))
+        # print(formateo_lista(resultado))
         #FALTA ESTA DEF
 
     if opcion_elegida == 3: #3: Ver que materias tengo que tener aprobadas, para cursar una materia
         resultado = materia_online_id(materias)
-        print(formateo_lista(resultado))
+        # print(formateo_lista(resultado))
 
     if opcion_elegida == 4: #4: Ver listado de materias online
         resultado = listas_de_materias(materias)
-        print(formateo_lista(resultado))
+        # print(formateo_lista(resultado))
 
     if opcion_elegida == 5: #5: Ver si una materia esta disponible para cursar online
         resultado = listas_de_materias(materias)
-        print(formateo_lista(resultado))
+        # print(formateo_lista(resultado))
+
+
+main(materias)
